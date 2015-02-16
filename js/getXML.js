0585700cd7248694e800bad2621c0175472ff934
Lainy VINCENT <lainy971@hotmail.com>
2015-02-16 14:28:04 +0100

    Création d'un fichier de test.html pour nous exercer au parcours du fichier XML récupéré. Quelques modifications de style ont été apportées et agencement du code.

--- js/getXML.js
+++ js/getXML.js
@@ -1,8 +1,17 @@
 var xhr = new XMLHttpRequest({mozSystem: true});
-			xhr.open("GET", "http://xmltv.dtdns.net/alacarte/ddl?fichier=/xmltv_site/xmlPerso/indianboy.xml", true);
-			xhr.onreadystatechange = function() {
-			  if (xhr.readyState == 4) {
-				$("#log").html(xhr.responseText);
-			  }
-			}
-			xhr.send();
+xhr.open("GET", "http://xmltv.dtdns.net/alacarte/ddl?fichier=/xmltv_site/xmlPerso/indianboy.xml", true);
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+                              
+        var channels = xhr.responseXML.getElementsByTagName("display-name");
+        var programmes = xhr.responseXML.getElementsByTagName("channel");
+        
+        $("#channel").html(programmes[0]);
+        //$("#title").html(channels);
+        //$("#description").html(channels);
+			
+    }
+}
+
+xhr.send();
