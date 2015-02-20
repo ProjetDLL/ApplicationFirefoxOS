57b83ab851d08148de1ad082841bae4fe42e1aa1
Lainy VINCENT <lainy971@hotmail.com>
2015-02-20 20:15:10 +0100

    Affichage des 3 chaînes dans les différentes div. A titre de test, j'ai seulement affiché le premier programme correspondant à chaque chaîne (d'où l'instruction "break" dans le if).
    Je continue à travailler sur la boucle pour afficher les programmes suivants.

--- js/getXML.js
+++ js/getXML.js
@@ -4,40 +4,102 @@ xhr.open("GET", "http://xmltv.dtdns.net/alacarte/ddl?fichier=/xmltv_site/xmlPers
 
 xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
+    	
+    	var channels = xhr.responseXML.getElementsByTagName("channel");//balise chaine - liste de toutes les chaines
          
-        // balise chaine
-        var channels = xhr.responseXML.getElementsByTagName("channel");//balise chaine - liste de toutes leschaines
-            var channelId = channels[0].getAttribute("id");
-            var channelName = channels[0].getElementsByTagName("display-name")[0].textContent;
+    	var wid = 0;
+    	var tran = 0;
+    	var divg;
+    	
+    	for(i = 0; i < channels.length; i++){
+         	
+            divg = document.createElement('div');
             
-        //balise programme
-        var programmes = xhr.responseXML.getElementsByTagName("programme");
-            var programmeId = programmes[0].getAttribute("channel");
-            var programmeStart = programmes[0].getAttribute("start");
-                var debut = "Le " + programmeStart.substring(6,8) + "/" + 
-                programmeStart.substring(4,6) + "/" + 
-                programmeStart.substring(0,4) + " a " + 
-                programmeStart.substring(8,10) + "h" + 
-                programmeStart.substring(10,12);
-        
-            var programmeTitre = programmes[0].getElementsByTagName("title")[0].textContent;
-            var programmeSousTitre = programmes[0].getElementsByTagName("sub-title")[0].textContent;
-            var programmeDesc = programmes[0].getElementsByTagName("desc")[0].textContent;
-            var programmeLength = programmes[0].getElementsByTagName("length");
-            var lenghtUnit = programmeLength[0].getAttribute("units");
+            //Positionnement de la div générale pour le défilement horizontal des chaînes
+            divg.setAttribute('data-index', i);
+            divg.style = 'width: 320px; left: ' + wid + 'px; transition-duration: 300ms; transform: translateX(' + 
+            tran + 'px);';
+    		
+            wid = wid - 320;
+            if(tran == 0){
+            	tran = tran + 320;
+            }
             
-        
-        
-        
-        //$("#channel").html(channels[0].getElementsByTagName("display-name")); //Nom chaine
-        $("#channel").html(channelName);
-        $("#title").html(programmeTitre);
-        $("#subtitle").html("("+programmeSousTitre+")");
-        $("#startANDduration").html(debut+" ("+programmeLength[0].textContent+" "+lenghtUnit+")");
-        $("#description").html(programmeDesc);
-        
+    		var dive = document.createElement('div');
+             dive.id = 'b';
+             
+            
+    	
+             	// balise chaine
+	            var channelId = channels[i].getAttribute("id");
+	            var channelName = channels[i].getElementsByTagName("display-name")[0].textContent;
+	            
+	            var tpl = document.getElementById('structure');
+	            
+	            var div = document.getElementsByClassName('swipe-wrap');
+	            
+	            var btn = tpl.content.cloneNode(true);
+	            
+	            
+	            //On insère le nom de la chaîne
+	            btn.getElementById('channel').textContent = channelName;
+	            
+		        //balise programme
+		        var programmes = xhr.responseXML.getElementsByTagName("programme");
+		        
+		        for(j = 0; j < programmes.length; j++){
+		        	
+		        	 var programmeId = programmes[j].getAttribute("channel");
+		        	 
+		        	 if(programmeId == channelId){
+		        		 var programmeStart = programmes[j].getAttribute("start");
+			                var debut = "Le " + programmeStart.substring(6,8) + "/" + 
+			                programmeStart.substring(4,6) + "/" + 
+			                programmeStart.substring(0,4) + " a " + 
+			                programmeStart.substring(8,10) + "h" + 
+			                programmeStart.substring(10,12);
+			        
+			            var programmeTitre = programmes[j].getElementsByTagName("title")[0].textContent;
+			            var programmeSousTitre = programmes[j].getElementsByTagName("sub-title")[0].textContent;
+			            var programmeDesc = programmes[j].getElementsByTagName("desc")[0].textContent;
+			            var programmeLength = programmes[j].getElementsByTagName("length");
+			            var lenghtUnit = programmeLength[0].getAttribute("units");
+			            
+			            break;
+		        	 }
+			            
+		        	
+		        }
+	            
+		        var tmp =  btn.getElementById('mainProgram').firstElementChild;
+		        
+		        //Insertion du titre du programme
+	           tmp.textContent = programmeTitre;
+	           
+	           //Voisin suivant
+	           tmp = tmp.nextElementSibling;
+	            
+		        //Insertion du sous-titre du programme
+	            tmp.textContent = "("+programmeSousTitre+")";
+	            
+		        //Voisin suivant
+		        tmp = tmp.nextElementSibling;
+		            
+			    //Insertion de la date, l'heure de début et la durée du programme
+		        tmp.textContent = debut + " (" + programmeLength[0].textContent + " " + lenghtUnit +")";
+		        
+			    //Insertion de la description du programme
+		        btn.getElementById('mainProgram').lastElementChild.textContent = programmeDesc;
+	            
+	            dive.appendChild(btn);
+	            
+	            divg.appendChild(dive);
+
+	            div[0].appendChild(divg);
+    	}
+    	
 			
-    }
+   }
 }
 
 xhr.send();
